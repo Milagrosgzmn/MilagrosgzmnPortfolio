116d3cf30484bfbda3300ce34cc2516253a637de
milagros <miliguzman0@gmail.com>
2023-11-16 17:26:04 -0300

    fix: icons styles

--- myportfolio/src/app/page.tsx
+++ myportfolio/src/app/page.tsx
@@ -5,6 +5,7 @@ import Avatar from '../../public/assets/27470372_7309700.jpg';
 import ContactComp from '@/components/contactShort/contactComp';
 import AboutComp from '@/components/aboutShort/aboutComp';
 import ProjectComp from '@/components/projectsShort/projectComp';
+import './styles.css';
 
 export default function Home() {
 
@@ -62,7 +63,7 @@ export default function Home() {
     }
   };
  return (
- <div className='snap-y snap-mandatory max-w-5xl w-4/5 mx-auto relative h-screen overflow-y-auto overflow-x-hidden scroll-smooth'>
+ <div className='sm:snap-y sm:snap-mandatory max-w-5xl w-4/5 mx-auto relative h-screen overflow-y-auto overflow-x-hidden scroll-smooth'>
     <section  style={sectionStyle} className='snap-center flex flex-col w-full items-center justify-evenly '>
       <div className=' flex flex-col-reverse md:flex-row  items-center justify-center'>
         <article className='p-2'>
@@ -79,20 +80,20 @@ export default function Home() {
         ;
       }} className='rounded-full text-xl border-2 border-solid border-sky-500 text-white bg-sky-500 py-4 px-6 font-semibold mt-2 hover:bg-white hover:text-sky-500'>Comenzar</button>
     </section>
-    <section style={sectionStyle} id='about' className='short-vers snap-center h-screen  mx-auto flex flex-col items-center'>
+    <section style={sectionStyle} id='about' className='short-vers  mx-auto flex flex-col items-center'>
         <h2 className='text-sky-500 w-full text-3xl font-bold pt-4'>Sobre mí</h2>
         <div className='w-10/12'>
         <AboutComp />
         </div>
       </section>
-      <section style={sectionStyle} className=' short-vers snap-center h-screen  mx-auto flex flex-col  '>
+      <section style={sectionStyle} className=' short-vers  mx-auto flex flex-col  '>
         <h2 className='text-sky-500  w-full text-3xl font-bold pt-4' >Proyectos</h2>
         <p className='pt-2 dark:text-white'>Proyectos que he realizado hasta ahora.</p>
         <div className=''>
         <ProjectComp/>
         </div>
       </section>
-      <section style={sectionStyle} className=' short-vers snap-center h-screen  mx-auto flex flex-col  '>
+      <section style={sectionStyle} className=' short-vers  mx-auto flex flex-col  '>
         <h2 className='text-sky-500  w-full text-3xl font-bold py-8'>Contacto</h2>
         <p className='p-4 font-medium  dark:text-white'>Si estás interesado/a en mi trabajo o tienes alguna pregunta, ¡no dudes en contactarme!</p>
         <div className='flex w-10/12 mx-auto items-center justify-evenly flex-col md:flex-row'>
